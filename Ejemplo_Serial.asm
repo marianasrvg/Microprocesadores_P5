98448e0f8424a1596a7ec90e9c57a8ce97516d3d
Mariana <mari.anasieveg2@gmail.com>
2017-11-07 14:02:01 -0600

    Optimización de funciones de LCD

--- Ejemplo_Serial.asm
+++ Ejemplo_Serial.asm
@@ -1,15 +1,16 @@
-          ; Ejemplo de transmisión serial
+      ; Ejemplo de transmisiï¿½n serial
 		  ; Primavera 2017 ITESO
+
 			ORG 00H
 			sjmp main;
 			ORG 40H
 main:       mov SCON,#01000000b   ; sm0 sm1 sm2 ren tb8 rb8 ti ri
-							      ; uart 8 bits banderas limpias
-			mov TMOD, #00100000b  ; G T!/C M1 M0 G T!/C M1 M0
-			mov TH1,#0FDH         ; T! autorecarga para baudrate de 9600 bps
+							                    ; uart 8 bits banderas limpias
+			mov TMOD, #00100000b        ; G T!/C M1 M0 G T!/C M1 M0
+			mov TH1,#0FDH                ; T! autorecarga para baudrate de 9600 bps
 			mov TL1,#(-3)
 			setb TR1
-			
+
 			mov A,#'H'
 imprime:	mov SBUF,A
 espera:		jnb TI, espera
@@ -18,22 +19,23 @@ espera:		jnb TI, espera
 			sjmp imprime
 			nop
 			nop
-			END  
-           ; Ejemplo de recepción serial
+			END
+
+       ; Ejemplo de recepciï¿½n serial
 		   ; Primavera 2017 ITESO
 
 /*
-            ORG 00H
+      ORG 00H
 			sjmp main;
 			ORG 40H
 main:       mov SCON,#01000000b   ; sm0 sm1 sm2 ren tb8 rb8 ti ri
-							      ; uart 8 bits banderas limpias
+							                    ; uart 8 bits banderas limpias
 			mov TMOD, #00100000b  ; G T!/C M1 M0 G T!/C M1 M0
 			mov TH1,#0FDH         ; T! autorecarga para baudrate de 9600 bps
 			mov TL1,#(-3)
 			setb TR1
 			setb REN
-			
+
 espera:		jnb RI, espera
             clr RI
             mov A,SBUF
@@ -41,8 +43,8 @@ espera:		jnb RI, espera
 			nop
 			nop
 			END  */
-			
-	/*		
+
+	/*
 	ORG 00H
 	JMP Main
 	ORG 0040H
@@ -53,36 +55,12 @@ Main:
 	MOV TL1,#0FDH
 	SETB TR1
 	MOV A,#30H
-Valida:	
+Valida:
 	JNB TI,$
 	CPL TI
 	MOV SBUF,A
 	INC A
 	SJMP Valida
-	
+
 	END
-*/	
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			 
-			
+*/
